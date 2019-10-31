caf72dd4cea137537085e9c2c0767d174d8f8eea
Student <student@eda-bc-06.localdomain>
2019-11-01 10:54:02 +1300

    Add get tasks for project & remove get projects func

--- routes/routes.js
+++ routes/routes.js
@@ -12,9 +12,18 @@ router.get('/', (req, res) => {
         })
     })
 })
+//don't really need this, same as below but without info of tasks todo.
+// router.get('/kanban/:id', (req, res) => {
+//     db.getProject(req.params.id)
+//     .then(project => {
+//         res.render('kanban', {
+//             project
+//         })
+//     })
+// })
 
 router.get('/kanban/:id', (req, res) => {
-    db.getProject(req.params.id)
+    db.getTasksForProject(req.params.id)
     .then(project => {
         res.render('kanban', {
             project
