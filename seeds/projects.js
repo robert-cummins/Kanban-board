769d0699b6550cf2c79e2417f539bb255c6710bc
robert-cummins <robert.k.cummins@gmail.com>
2019-11-01 09:29:21 +1300

    Update seeds

--- seeds/projects.js
+++ seeds/projects.js
@@ -6,6 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('projects').insert([
         {projectId: 1, name: 'Clean the house '},
+        {projectId: 2, name: 'Build a house'}
        
       ]);
     });
--- seeds/tasks.js
+++ seeds/tasks.js
@@ -11,7 +11,15 @@ exports.seed = function(knex) {
         {taskId: 104, task: 'CLean oven', project_id: 1},
         {taskId: 105, task: 'Clean bathroom', project_id: 1},
         {taskId: 106, task: 'Vacuum house', project_id: 1},
-        {taskId: 107, task: 'Make bed', project_id: 1}
+        {taskId: 107, task: 'Make bed', project_id: 1},
+
+        {taskId: 108, task: 'Lay foundation', project_id: 2},
+        {taskId: 109, task: 'Lay blocks for house', project_id: 2},
+        {taskId: 110, task: 'Build timber frame', project_id: 2},
+        {taskId: 111, task: 'Tile the roof', project_id: 2},
+        {taskId: 112, task: 'Plaster the walls', project_id: 2},
+        {taskId: 113, task: 'Add plumbing', project_id: 2},
+        {taskId: 114, task: 'Add electrics', project_id: 2},
       ]);
     });
 };
